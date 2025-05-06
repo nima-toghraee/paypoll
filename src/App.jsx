3d32222f5f83822fef5123075912be3973df4b0b
nima <toghraeen@gmail.com>
2025-05-06 15:52:18 +0330

    update userinfo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductCode from "./Pages/ProductCode";
 import User from "./Pages/User";
+import UserInfo from "./Pages/UserInfo";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ProductCode />} />
         <Route path="/user" element={<User />} />
+        <Route path="/userForm" element={<UserInfo />} />
       </Routes>
     </BrowserRouter>
   );
